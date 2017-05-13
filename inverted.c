1259a7d32ff2407d2d590d5aa609d20499271c66
ckhu <ckhu@users.noreply.github.com>
2017-05-14 01:26:41 +1000

    Create inverted.c

---
+++ inverted.c
@@ -0,0 +1,3 @@
+//reads data from a given collection of pages in collection.txt 
+//and generates an "inverted index" that provides a list (set) of 
+//urls for every word in a given collection of pages
