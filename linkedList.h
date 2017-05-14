e5bfa4519626bcaa43ea333ee513a810679349d6
declanscale <declanscale@gmail.com>
2017-05-14 13:12:39 +1000

    Update linkedList.h

--- linkedList.h
+++ linkedList.h
@@ -1,6 +1,11 @@
-typedefine struct URLlist{
+typedefine struct urlList{
 
  char *url
  struct urlList* next
   
 }UL;
+
+
+struct node *create_node(int data, struct urlList *next);
+struct node *last(struct urlList *head);
+struct node *append(struct urlList *head, int value);
