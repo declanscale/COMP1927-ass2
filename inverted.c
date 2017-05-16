05cd6b150bb725bbb12bd37905da95815060bfff
ckhu <ckhu@users.noreply.github.com>
2017-05-16 16:22:55 +1000

    Update inverted.c

--- inverted.c
+++ inverted.c
@@ -1,3 +1,14 @@
 //Read data from a given collection of pages in collection.txt 
 //and generates an "inverted index" that provides a list (set) of 
 //urls for every word in a given collection of pages
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "readData.h"
+#include "linkedList.h"
+#include "graph.h"
+#include "BSTree.h"
+
+GetInvertedList(urlNode list) {
+  
+}
